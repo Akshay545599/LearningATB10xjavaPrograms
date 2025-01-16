ced34fd9010bb57d7f34cda3560e37c2d0a963db
akshu <aksh5599@gmail.com>
2025-01-16 14:52:31 +0530

    my 9th commit

---
+++ src/Lab008.java
@@ -0,0 +1,9 @@
+public class Lab008 {
+    public static void main(String[] args) {
+        String name = "akshay";
+        String name2 = name.replace("a", "b");
+        System.out.println(name2);
+
+    }
+
+    }
