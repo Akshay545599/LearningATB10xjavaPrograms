c5b4c117d323f8c1cf93baf74262a61771280843
akshu <aksh5599@gmail.com>
2025-01-16 14:11:16 +0530

    my 7th commit

---
+++ src/Lab006.java
@@ -0,0 +1,17 @@
+public class Lab006 {
+    public static void main(String[] args) {
+        String name1 ="Akshay";
+        String name2 ="Swapnil";
+        String name3=name1+ " and "+name2;
+
+
+        {
+            System.out.println(name3);
+        }
+    }
+    }
+
+
+
+
+
