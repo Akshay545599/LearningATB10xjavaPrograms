56a5d84675624ea621c70d7838b49adecfe2f04d
akshu <aksh5599@gmail.com>
2025-01-23 17:20:44 +0530

    Lab_073_De_P2

---
+++ src/ex_07_Increment_Decrement_Op/Lab_073_De_P2.java
@@ -0,0 +1,14 @@
+package ex_07_Increment_Decrement_Op;
+
+public class Lab_073_De_P2 {
+    public static void main(String[] args) {
+        int a= 10;
+        int result = --a; // 9 (reduce by 1)
+        System.out.println(result);
+        System.out.println(a);
+    }
+
+
+
+
+}
