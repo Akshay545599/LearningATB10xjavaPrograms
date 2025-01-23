8ed882e99ffe8deaf1abdf122854b2ae7883846e
akshu <aksh5599@gmail.com>
2025-01-23 10:25:47 +0530

    Lab_010_Variables

---
+++ src/ex_02_JavaBasics/Lab_010_Variables.java
@@ -0,0 +1,15 @@
+package ex_02_JavaBasics;
+
+public class Lab_010_Variables {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+
+    public void main(String args) {
+
+    }
+
+
+
+}
