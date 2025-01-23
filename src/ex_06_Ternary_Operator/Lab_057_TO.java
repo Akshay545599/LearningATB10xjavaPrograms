0827e799f36428e999e229218790a2ea1d35a8ac
akshu <aksh5599@gmail.com>
2025-01-23 16:18:22 +0530

    Lab_057_TO

---
+++ src/ex_06_Ternary_Operator/Lab_057_TO.java
@@ -0,0 +1,12 @@
+package ex_06_Ternary_Operator;
+
+public class Lab_057_TO {
+    public static void main(String[] args) {
+        // result = condition ? expression1 : expression2;
+        int age = 10;
+        String canIVote =  age >= 18 ? "Yes, You can vote" : "No, You can't Vote";
+        System.out.println(canIVote);
+    }
+
+
+}
