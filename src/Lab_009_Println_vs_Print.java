074fa3f57132aeb28bf625d6c9e3b8650868f13c
akshu <aksh5599@gmail.com>
2025-01-23 10:20:41 +0530

    Lab_009_Println_vs_Prin

---
+++ src/Lab_009_Println_vs_Print.java
@@ -0,0 +1,12 @@
+public class Lab_009_Println_vs_Print {
+    public static void main(String[] args) {
+//        System.out.println("Hello World1");
+//        System.out.println("Hello World2");
+
+        System.out.print("Hello World3");
+        System.out.print("Hello World4");
+    }
+
+
+
+}
