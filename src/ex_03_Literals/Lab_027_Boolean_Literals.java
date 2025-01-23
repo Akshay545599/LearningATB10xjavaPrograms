9e05a8eb87e6ae2df699db0048ffb5e1da45b4fb
akshu <aksh5599@gmail.com>
2025-01-23 14:14:55 +0530

    Lab_027_Boolean_Literals

---
+++ src/ex_03_Literals/Lab_027_Boolean_Literals.java
@@ -0,0 +1,17 @@
+package ex_03_Literals;
+
+public class Lab_027_Boolean_Literals {
+    public static void main(String[] args) {
+        boolean is_married = true;
+        boolean is_married_amit = false;
+        // First Type - Boolean  values - true or false.
+
+        int age = 78;
+        int marks_ece = -1;
+        int lambo = 0;
+
+    }
+
+
+
+}
