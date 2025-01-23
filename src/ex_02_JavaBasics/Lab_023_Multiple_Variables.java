3b4138e48df8180eb1cd9da2b7aaa32797fb3780
akshu <aksh5599@gmail.com>
2025-01-23 11:18:41 +0530

    Lab_023_Multiple_Variables

---
+++ src/ex_02_JavaBasics/Lab_023_Multiple_Variables.java
@@ -0,0 +1,15 @@
+package ex_02_JavaBasics;
+
+public class Lab_023_Multiple_Variables {
+
+        public static void main(String[] args) {
+            int a = 10, b = 23, c = 45;
+
+            int d = 10;// int d = 23;
+
+
+        }
+
+
+
+}
