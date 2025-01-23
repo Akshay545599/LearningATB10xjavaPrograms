84fe882d1f0b6609ee57f83ddb117e2b52b4cf9f
akshu <aksh5599@gmail.com>
2025-01-23 10:02:09 +0530

    Lab_001_Shortcut_Comments_Multi

---
+++ src/ex_01_Java_Basic/Lab_004_Comments_Multi.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basic;
+
+public class Lab_004_Comments_Multi {
+    public static void main(String[] args) {
+        /**
+         * Author : Akshay patil
+         * Course :  ATB10x
+         *
+         */
+        System.out.println("Multi Comment");
+    }
+
+
+}
