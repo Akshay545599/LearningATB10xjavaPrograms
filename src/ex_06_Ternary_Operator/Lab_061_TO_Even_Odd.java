c01afa5588912dea7d79ee98a1fba79bdf655f12
akshu <aksh5599@gmail.com>
2025-01-23 16:29:59 +0530

    Lab_061_TO_Even_Odd

---
+++ src/ex_06_Ternary_Operator/Lab_061_TO_Even_Odd.java
@@ -0,0 +1,19 @@
+package ex_06_Ternary_Operator;
+
+public class Lab_061_TO_Even_Odd {
+    public static void main(String[] args) {
+
+        int num = 10;
+        String result = (num%2==0) ? "Even" : "Odd";
+        System.out.println(result);
+
+
+
+
+
+
+    }
+
+
+
+}
