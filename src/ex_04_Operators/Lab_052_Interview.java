c11ab2a3a1f258858366af9f28b7017d74c681fd
akshu <aksh5599@gmail.com>
2025-01-23 16:03:02 +0530

    Lab_052_Interview

---
+++ src/ex_04_Operators/Lab_052_Interview.java
@@ -0,0 +1,15 @@
+package ex_04_Operators;
+
+public class Lab_052_Interview {
+    public static void main(String[] args) {
+        short s = 10;
+        char c = 'A'; //A -> 65
+        System.out.println(c+s);
+        System.out.println(c);
+        // When you do the + concat then only ASCII values are used.
+
+    }
+
+
+
+}
