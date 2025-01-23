d3add56806cdea00d9d1696158426fa8a93ac5ba
akshu <aksh5599@gmail.com>
2025-01-23 15:27:50 +0530

    Lab_042_Interview

---
+++ src/ex_04_Operators/Lab_042_Interview.java
@@ -0,0 +1,13 @@
+package ex_04_Operators;
+
+public class Lab_042_Interview {
+    public static void main(String[] args) {
+        System.out.println(!(10>20));
+        //System.out.println((30>90)!);
+        System.out.println(!!!!(30>90));
+    }
+
+
+
+
+}
