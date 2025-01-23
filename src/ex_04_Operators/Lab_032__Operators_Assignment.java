5abcbc48832274e108a7c9f4a4d76959bece0d9e
akshu <aksh5599@gmail.com>
2025-01-23 14:46:31 +0530

    Lab_032__Operators_Assignment

---
+++ src/ex_04_Operators/Lab_032__Operators_Assignment.java
@@ -0,0 +1,20 @@
+package ex_04_Operators;
+
+public class Lab_032__Operators_Assignment {
+    public static void main(String[] args) {
+        // Assignment operator
+
+        int age = 90;
+
+        // int -> data type
+        // age -> identifier
+        // 90 -> Literal
+        // = -> ? operator
+
+        // RHV to the LH
+
+    }
+
+
+
+}
