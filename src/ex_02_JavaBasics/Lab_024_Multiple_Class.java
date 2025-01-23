18995fac0f99c990a62e8df7e12907a1e1ad9a76
akshu <aksh5599@gmail.com>
2025-01-23 11:22:25 +0530

    Lab_024_Multiple_Class

---
+++ src/ex_02_JavaBasics/Lab_024_Multiple_Class.java
@@ -0,0 +1,21 @@
+package ex_02_JavaBasics;
+
+public class Lab_024_Multiple_Class {
+
+        public static void main(String[] args) {
+
+        }
+    }
+
+    class A {
+
+    }
+
+    class B {
+    }
+
+    class C {
+    }
+
+
+
