bada883f2fece1c9a3e01d8f77a8c543d81c35ff
akshu <aksh5599@gmail.com>
2025-01-23 10:30:35 +0530

    Lab_012_Variables

---
+++ src/ex_02_JavaBasics/Lab_012_Variables.java
@@ -0,0 +1,17 @@
+package ex_02_JavaBasics;
+
+public class Lab_012_Variables {
+    public static void main(String[] args) {
+        int a = 10;
+        // data type variable_name =  variable_value
+
+        int age = 65;
+        age = 65 + 1;
+        System.out.println(age);
+
+
+    }
+
+
+
+}
