804d85f94f9bc585166bb921c068f07a720db1bf
akshu <aksh5599@gmail.com>
2025-01-23 10:16:15 +0530

    Lab_008_Math_Exception

---
+++ src/ex_01_Java_Basic/Lab_008_Math_Exception.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class Lab_008_Math_Exception {
+    public static void main(String[] args) {
+        System.out.println(10/0);
+        // java.lang.ArithmeticException
+    }
+
+
+
+}
