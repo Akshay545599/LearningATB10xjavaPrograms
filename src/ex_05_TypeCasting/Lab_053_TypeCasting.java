44271c12ecacda73848d7f1fea1c85aa5b7c6f6a
akshu <aksh5599@gmail.com>
2025-01-23 16:06:01 +0530

    Lab_053_TypeCasting

---
+++ src/ex_05_TypeCasting/Lab_053_TypeCasting.java
@@ -0,0 +1,12 @@
+package ex_05_TypeCasting;
+
+public class Lab_053_TypeCasting {
+    public static void main(String[] args) {
+        byte b = 10;
+//        int a = b; // Valid - > Implicit Casting- Automatically
+        int a = (int)b; // Valid - > Explicit Casting- Automatically
+
+    }
+
+
+}
