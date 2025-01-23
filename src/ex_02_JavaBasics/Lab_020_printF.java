2945d9931ae1ea83355ab049ffeae0e51f87d4b7
akshu <aksh5599@gmail.com>
2025-01-23 11:09:24 +0530

    Lab_020_printF

---
+++ src/ex_02_JavaBasics/Lab_020_printF.java
@@ -0,0 +1,24 @@
+package ex_02_JavaBasics;
+
+public class Lab_020_printF {
+    public static void main(String[] args) {
+
+        int a = 10;
+        System.out.printf("Your variable name is  %d",a);
+        // %d -> int, byte, long, short - data
+        // %s
+        // %f float, %b boolean
+
+        byte b = 10;
+        System.out.println();
+        System.out.printf("%d",b);
+
+
+
+    }
+
+
+
+
+
+}
