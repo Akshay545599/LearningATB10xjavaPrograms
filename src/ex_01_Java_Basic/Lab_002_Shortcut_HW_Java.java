cf169c92a351ad93df9d1e5ea3cfa8a1a7a9adf1
akshu <aksh5599@gmail.com>
2025-01-23 09:52:57 +0530

    Lab_001_Shortcut_HW_Java

---
+++ src/ex_01_Java_Basic/Lab_002_Shortcut_HW_Java.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class Lab_002_Shortcut_HW_Java {
+    public static void main(String[] args) {
+        System.out.println("Amol");
+    }
+
+    public static void main() {
+        System.out.println("Amol");
+    }
+
+
+}
