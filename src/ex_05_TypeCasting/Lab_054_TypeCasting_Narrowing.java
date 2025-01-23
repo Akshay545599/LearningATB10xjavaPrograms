ea648174cf00bd57d419ba464c93e0143021b823
akshu <aksh5599@gmail.com>
2025-01-23 16:08:05 +0530

    Lab_054_TypeCasting_Narrowing

---
+++ src/ex_05_TypeCasting/Lab_054_TypeCasting_Narrowing.java
@@ -0,0 +1,13 @@
+package ex_05_TypeCasting;
+
+public class Lab_054_TypeCasting_Narrowing {
+    public static void main(String[] args) {
+        int val  = 200;
+//        byte b = val; // // Invalid - Implicit Casting narrowing is not allowed
+        byte b = (byte)val; // // // Valid -> Explicit Casting -  allowed
+        // Data Loss:
+    }
+
+
+
+}
