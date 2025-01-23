4811ace3c254591094e977323409ea0d820e4d7f
akshu <aksh5599@gmail.com>
2025-01-23 15:10:28 +0530

    Lab_037_Logical_Operators

---
+++ src/ex_04_Operators/Lab_037_Logical_Operators.java
@@ -0,0 +1,16 @@
+package ex_04_Operators;
+
+public class Lab_037_Logical_Operators {
+    public static void main(String[] args) {
+        boolean a = true;
+        System.out.println(!a);
+        boolean b = true;
+        System.out.println(!!b);
+        boolean c= true || false;
+        System.out.println(c);
+
+    }
+
+
+
+}
