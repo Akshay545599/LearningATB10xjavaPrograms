ffed15e48bebb8fbc7307288a703424fae68e8a6
akshu <aksh5599@gmail.com>
2025-01-23 15:53:19 +0530

    Lab_050_Interview

---
+++ src/ex_04_Operators/Lab_050_Interview.java
@@ -0,0 +1,15 @@
+package ex_04_Operators;
+
+public class Lab_050_Interview {
+    public static void main(String[] args) {
+        char a1 = 'A'; // ASCII -> 65
+        char b1 = 'B'; // 66
+        System.out.println(a1 + b1); //AB
+        // a1+b1 - 65+66 -> 131
+        // Rule - concatenation of Strings -> char -> int calculation
+        // a1+b1 - 65+66 -> 131
+        System.out.println(a1+b1);
+    }
+
+
+}
