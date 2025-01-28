63b5cd1f7a55f6faaf861721307b06280e7f33b0
akshu <aksh5599@gmail.com>
2025-01-28 22:28:48 +0530

    exersize 12

---
+++ src/ex_12_DoWhile/Lab_118.java
@@ -0,0 +1,21 @@
+package ex_12_DoWhile;
+
+public class Lab_118 {
+    public static void main(String[] args) {
+        int a = 0;
+//        while( a< 0){
+//            System.out.println(a);
+//            a++;
+//        }
+
+        do {
+            System.out.println(a);
+            a++;
+        } while (a < 0);
+
+
+    }
+
+
+
+}
