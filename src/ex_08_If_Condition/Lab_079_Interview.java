c5f7ba5c8ddc73e6f3aee946e96540290e0223d2
akshu <aksh5599@gmail.com>
2025-01-28 20:48:50 +0530

    exersize 8

---
+++ src/ex_08_If_Condition/Lab_079_Interview.java
@@ -0,0 +1,17 @@
+package ex_08_If_Condition;
+
+public class Lab_079_Interview {
+    public static void main(String[] args) {
+        boolean b = !true;
+        if(b){
+            System.out.println("b is true!");
+        }
+        else{
+            System.out.println("b is not true!");
+        }
+    }
+
+
+
+
+}
---
+++ src/ex_08_If_Condition/Lab_080_Interview.java
@@ -0,0 +1,18 @@
+package ex_08_If_Condition;
+
+public class Lab_080_Interview {
+    public static void main(String[] args) {
+        boolean a = true;
+        a = !a;
+        if (2 + 2 < 4) {
+            System.out.println("Haha");
+        }
+        System.out.println("End of program!");
+    }
+
+
+
+
+
+
+}
