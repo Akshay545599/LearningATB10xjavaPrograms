0cdeb071ba31aa79ab0fbfc7a77c93b76fc1992b
akshu <aksh5599@gmail.com>
2025-02-06 13:39:33 +0530

    exersize 15

--- src/Lab006.java
+++ src/Lab006.java
@@ -2,12 +2,12 @@ public class Lab006 {
     public static void main(String[] args) {
         String name1 ="Akshay";
         String name2 ="Swapnil";
-        String name3=name1+ " and "+name2;
+        String name3=name1+ "\n" +name2;
 
 
-        {
+        
             System.out.println(name3);
-        }
+
     }
     }
 
