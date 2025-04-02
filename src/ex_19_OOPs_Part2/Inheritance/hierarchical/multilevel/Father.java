4a322b120c72064c7e2461b6b89388eea13197f0
akshu <aksh5599@gmail.com>
2025-04-02 10:20:03 +0530

    exersize 18

---
+++ src/ex_19_OOPs_Part2/Inheritance/hierarchical/multilevel/Father.java
@@ -0,0 +1,15 @@
+package ex_19_OOPs_Part2.Inheritance.hierarchical.multilevel;
+
+public class Father extends GrandFather{
+
+
+//    void home(){
+//        System.out.println("Father Home");
+//    }
+
+    void extra(){
+        System.out.println("Extra");
+    }
+
+
+}
