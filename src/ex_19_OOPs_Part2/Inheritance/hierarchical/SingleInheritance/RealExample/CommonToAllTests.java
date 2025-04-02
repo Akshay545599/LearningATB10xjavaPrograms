11090bac14ea278ed4a878fa952ee06c726a3c8c
akshu <aksh5599@gmail.com>
2025-04-02 10:14:28 +0530

    exersize 18

---
+++ src/ex_19_OOPs_Part2/Inheritance/hierarchical/SingleInheritance/RealExample/CommonToAllTests.java
@@ -0,0 +1,24 @@
+package ex_19_OOPs_Part2.Inheritance.hierarchical.SingleInheritance.RealExample;
+
+public class CommonToAllTests {
+
+        void startBrowser(){
+            System.out.println("Starting a Browser!!");
+        }
+
+        void closeBrowser() {
+            System.out.println("Closing Browser!!");
+        }
+
+        void readExcelFile(){
+            System.out.println("Reading File");
+        }
+
+        void readDataBaseFile(){
+            System.out.println("Reading File");
+        }
+
+
+
+
+    }
