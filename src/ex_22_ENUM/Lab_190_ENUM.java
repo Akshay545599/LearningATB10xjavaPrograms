c250d24a118bf4a3db8d1df569c9ac64892db5b9
akshu <aksh5599@gmail.com>
2025-04-03 14:07:33 +0530

    exersize 22

---
+++ src/ex_22_ENUM/Lab_190_ENUM.java
@@ -0,0 +1,14 @@
+package ex_22_ENUM;
+
+public class Lab_190_ENUM {
+    public static void main(String[] args) {
+        System.out.println(Day.SUNDAY);
+    }
+}
+
+// Constants list
+enum Day{
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+
+
+}
