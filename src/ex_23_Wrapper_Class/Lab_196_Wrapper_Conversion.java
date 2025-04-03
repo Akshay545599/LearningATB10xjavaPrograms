bf060d1e16578552443416ef2ec5f4db1b950daf
akshu <aksh5599@gmail.com>
2025-04-03 14:11:51 +0530

    exersize 23

---
+++ src/ex_23_Wrapper_Class/Lab_196_Wrapper_Conversion.java
@@ -0,0 +1,28 @@
+package ex_23_Wrapper_Class;
+
+public class Lab_196_Wrapper_Conversion {
+    public static void main(String[] args) {
+        String num = "10";
+        int aaaa = 10;
+
+        // String -> Wrapper conversion
+        Integer a = Integer.parseInt(num);
+
+        // String to Primitive
+        int a3 = Integer.parseInt(num);
+        // Integer.max(3,4);
+
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
+        // a  <Integer.MAX_VALUE
+
+
+        Integer aa = Integer.valueOf("10");
+        System.out.println(aa);
+
+
+    }
+
+
+}
