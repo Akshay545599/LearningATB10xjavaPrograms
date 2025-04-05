e2869229e558ca20216bafed2f4bf3fd2afdd041
akshu <aksh5599@gmail.com>
2025-04-05 16:26:40 +0530

    exersize 26

---
+++ src/ex_26_Collection_Framework/SET/Lab_233_SET.java
@@ -0,0 +1,33 @@
+package ex_26_Collection_Framework.SET;
+
+import java.util.*;
+
+public class Lab_233_SET {
+    public static void main(String[] args) {
+        Set hs = new HashSet();
+        Set lhs = new LinkedHashSet();
+        Set ts = new TreeSet();
+
+        hs.add("Pramod");
+        hs.add("Pramod");
+        hs.add("pramod");
+        hs.add("dramod");
+        System.out.println(hs);
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
